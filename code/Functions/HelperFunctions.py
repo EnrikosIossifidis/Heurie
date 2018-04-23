7995b95d4ba48b0290420745eeb48f1594f16b2c
PelleGroot <pellegroot@gmail.com>
2018-04-23 16:19:42 +0200

    removed .vscode, added gitignore and started on the helperfunctions

---
+++ code/Functions/HelperFunctions.py
@@ -0,0 +1,20 @@
+import csv
+from classes import House
+
+# TODO: begin gemaakt, moet nog inkomen met de syntax. Return list of houses and batteries
+class ImportHouses
+    # load data and connect
+    with open('data/wijk2_huizen.csv', 'rb') as f:
+        reader = csv.reader(f)
+        items = list(reader)
+        houses = []
+
+        for item in items:
+        
+            houses.append(house)    
+        
+    
+class ImportBatteries
+    with open('data/wijk2_batterijen.csv', 'rb') as f:
+        reader = csv.reader(f)
+        # batteries = list(reader)
