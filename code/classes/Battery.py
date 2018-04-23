17117adf3776ac9d776a1536591c6d7a9e9905b3
PelleGroot <pellegroot@gmail.com>
2018-04-23 14:01:46 +0200

    Added two classes

---
+++ code/classes/Battery.py
@@ -0,0 +1,6 @@
+class Battery:
+    int x
+    int y
+    float maxCapacity
+    float curCapacity
+    list House
---
+++ code/classes/House.py
@@ -0,0 +1,6 @@
+class House:
+    def __init__(self):
+        self.x
+        self.y
+        self.cap
+        self.batterij
