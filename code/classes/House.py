954466642ab52baae8e050de6241f96575f78ea7
EnrikosIossifidis <enrikosi013@gmail.com>
2018-04-23 14:05:47 +0200

    Changed House class

--- code/classes/House.py
+++ code/classes/House.py
@@ -1,6 +1,6 @@
 class House:
-    def __init__(self):
-        self.x
-        self.y
-        self.cap
-        self.batterij
+    def __init__(self, input_element):
+        self.x = 0 
+        self.y = 0
+        self.cap = 0.0
+        self.batterij = 0.0
