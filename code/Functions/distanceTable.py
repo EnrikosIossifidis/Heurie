c6f674c6808b566d0209c276785cb2fa34d86d6d
EnrikosIossifidis <enrikosi013@gmail.com>
2018-04-24 14:46:21 +0200

    Minor adjustment distanceTable

--- code/Functions/distanceTable.py
+++ code/Functions/distanceTable.py
@@ -4,8 +4,7 @@ import numpy as np
 def makeDistanceTable(batteries, houses):
     distanceTable = np.array(range(len(batteries) + 1))
     for house in houses:
-        row = []
-        row.append(house.idHouse)
+        row = [house.idHouse]
         for bat in batteries:
             row.append(abs((int(house.x)-int(bat.x)))+abs((int(house.y)-int(bat.y))))
         np.asarray(row)
--- code/main.py
+++ code/main.py
@@ -6,8 +6,8 @@ from Functions.helperfunctions import importBatteries
 from classes.Battery import Battery
 from classes.House import House
 
-houses = importHouses(r"C:\Users\enrik\Documents\GitHub\Heurie\data\wijk1_huizen.csv")
-batteries = importBatteries(r"C:\Users\enrik\Documents\GitHub\Heurie\data\wijk1_batterijen.csv")
+houses = importHouses(r"C:\Users\enrik\Documents\GitHub\Heurie\data\wijk2_huizen.csv")
+batteries = importBatteries(r"C:\Users\enrik\Documents\GitHub\Heurie\data\wijk2_batterijen.csv")
 
 makeDistanceTable(batteries, houses)
 
