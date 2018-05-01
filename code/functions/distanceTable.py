2be388dc3b7d7ae4cc7bc5802fe37d0243f60a3c
EnrikosIossifidis <enrikosi013@gmail.com>
2018-05-01 19:05:51 +0200

    Commented distancetable

--- code/functions/distanceTable.py
+++ code/functions/distanceTable.py
@@ -2,11 +2,20 @@ import csv
 import numpy as np 
 
 def makeDistanceTable(env):
+
+    # create column for each battery
     distancetable = np.array(range(len(env.batteries) + 1))
+
+    # append idHouse to left column
     for house in env.houses:
         row = [house.idHouse]
+
+        # append distance to each battery for every house
         for bat in env.batteries:
             row.append(abs(house.x-bat.x)+abs(house.y-bat.y))
+
+        # convert list into numpy array and add each house
         np.asarray(row)
         distancetable = np.vstack([distancetable, row])
+
     return(distancetable)
