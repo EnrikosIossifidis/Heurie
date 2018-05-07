15098814799be67c7d2b1bd8266cf1d1ada42bfc
PelleGroot <pellegroot@gmail.com>
2018-05-07 16:42:09 +0200

    pseudocode to hillclimber and deleted the brainstorm

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -1,8 +1,19 @@
 from classes.model import Model
 
-def hillClimber(model, env):
+def hillClimber(model, env, iteraties):
     curModel = model
     batteries = curModel.modelBatteries
 
-
-
+    # create list of costs
+    # do random
+    # get randommodel.cost
+    # create model (copy of random model)
+    # for iterations
+    #   do function(model)
+    #       switch two houses in model
+    #       calculate costs
+    #       compare model.cost to random.cost
+    #       return the cheapest
+    #       append cheapest to list
+    #   if cheapest < random.cost
+    #       model = cheapest model
