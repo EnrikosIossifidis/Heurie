a5d347a0f1ee19d9c2a6a75ddadfd88411e9ed46
PelleGroot <pellegroot@gmail.com>
2018-05-22 19:46:44 +0200

    edited hillcimber, it just needs a little tweek to fit the last house into a battery

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -14,7 +14,6 @@ def hillClimber(env, model, iterations, moves):
 
     # run the algorithm for the amount of iterations given
     for i in range(0, iterations):
-        
         check = False
         while check == False:
             climbedModel = climberModel
@@ -24,7 +23,8 @@ def hillClimber(env, model, iterations, moves):
 
         # calculate the costs of the returned model
         climbedModel.calculateCosts(env.distanceTable)
-
+        print(boundModel.cost)
+        print(climbedModel.cost)
         # compare the costs to the bound state
         if (climbedModel.cost < boundModel.cost):
 
@@ -46,23 +46,44 @@ def climbHill(model):
 
     # get a random battery
     randomBatteries =(random.randint(0, len(batteries)-1), random.randint(0, len(batteries)-1))
+    batterySendingHouse = batteries[randomBatteries[0]]
 
-    if batteries[randomBatteries[0]].houses != []:
+    if allowSending(batterySendingHouse):
         # set the upperbounds for the houses randomizer
-        setUpperboundBattery1 = len(batteries[randomBatteries[0]].houses)
+        setUpperboundBattery1 = len(batterySendingHouse.houses)
 
         # get a random house
         randomHouseId = random.randint(0, (setUpperboundBattery1 - 1))
 
         # get the houses on the random places
-        house1 = batteries[randomBatteries[0]].houses[randomHouseId]
+        house1 = batterySendingHouse.houses[randomHouseId]
 
-        # add the house to the other battery
-        batteries[randomBatteries[1]].houses.append(house1)
-        houses = batteries[randomBatteries[0]].houses
-        houses.pop(randomHouseId)
-        batteries[randomBatteries[0]].houses = houses
+        # get battery receiving the house
+        batteryReceivingHouse = batteries[randomBatteries[1]]
+
+        if allowPlacement(batteryReceivingHouse):
+            # add the house to the other battery
+            batteries[randomBatteries[1]].houses.append(house1)
+            houses = batterySendingHouse.houses
+            houses.pop(randomHouseId)
+            batteries[randomBatteries[0]].houses = houses
 
     # return the model
     returnModel = Model(batteries)
     return returnModel
+
+def allowPlacement(battery):
+    length = len(battery.houses)
+    if length < 35:
+        return True
+    else:
+        # print("Not accepted in allow Placement")
+        return False
+
+def allowSending(battery):
+    length = len(battery.houses)
+    if length > 25:
+        return True
+    else:
+        # print("Not accepted in allow Sending")
+        return False    
