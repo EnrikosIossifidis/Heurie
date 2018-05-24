de4f39b2ca66e53b8ce5114bfd5cce57bec95a4b
Pelle Groot <pelle.groot@globalorange.nl>
2018-05-24 16:45:47 +0200

    combined two types of hillclimbers into one big optional hillclimber

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -3,7 +3,7 @@ from algorithms.runrandom import runRandom
 import random
 import copy
 
-def hillClimber(env, model, iterations, moves):
+def hillClimber(env, model, iterations, chooseConstraints, mutation, moves):
 
     # create a list with all the lowest costs per iteration for plotting purposes
     costs = []
@@ -14,12 +14,7 @@ def hillClimber(env, model, iterations, moves):
 
     # run the algorithm for the amount of iterations given
     for i in range(0, iterations):
-        check = False
-        while check == False:
-            climbedModel = climberModel
-            for j in range(0, moves):
-                 climbedModel = climbHill(climbedModel)  
-            check = climbedModel.checkValidity()
+        climbedModel = chooseClimbModel(chooseConstraints, climberModel, mutation, iterations)
 
         # calculate the costs of the returned model
         climbedModel.calculateCosts(env.distanceTable)
@@ -38,7 +33,13 @@ def hillClimber(env, model, iterations, moves):
 
     return boundModel
 
-def climbHill(model):
+def switchOrMove(mutation, climberModel):
+    if mutation == 1:
+        return climbHillMoveHouse(climberModel)
+    elif mutation == 2:
+        return climbHillSwitchHouse(climberModel)
+
+def climbHillMoveHouse(model):
 
     # get the batteries from the model
     batteries = model.modelBatteries
@@ -71,6 +72,33 @@ def climbHill(model):
     returnModel = Model(batteries)
     return returnModel
 
+def climbHillSwitchHouse(model):
+
+    # get the batteries from the model
+    batteries = model.modelBatteries
+
+    # get a random battery
+    randomBatteries =(random.randint(0, len(batteries)-1), random.randint(0, len(batteries)-1))
+
+    # set the upperbounds for the houses randomizer
+    setUpperboundBattery1 = len(batteries[randomBatteries[0]].houses)
+    setUpperboundBattery2 = len(batteries[randomBatteries[1]].houses)
+
+    # get a random house
+    randomHouses = (random.randint(0, (setUpperboundBattery1 - 1)), random.randint(0, (setUpperboundBattery2 - 1)))
+
+    # get the houses on the random places
+    house1 = batteries[randomBatteries[0]].houses[randomHouses[0]]
+    house2 = batteries[randomBatteries[1]].houses[randomHouses[1]]
+
+    # switch the houses with each other
+    batteries[randomBatteries[0]].houses[randomHouses[0]] = house2
+    batteries[randomBatteries[1]].houses[randomHouses[1]] = house1
+
+    # return the model
+    returnModel = Model(batteries)
+    return returnModel
+
 def allowPlacement(battery):
     length = len(battery.houses)
     if length < 35:
@@ -86,3 +114,22 @@ def allowSending(battery):
     else:
         # print("Not accepted in allow Sending")
         return False    
+
+def chooseClimbModel(chooseConstraints, climberModel, mutation, moves):
+    if chooseConstraints == 1:
+        return climbWithConstraints(climberModel, mutation, moves)
+    elif chooseConstraints == 2:
+        return climbWithoutConstraints(climberModel, mutation)
+
+def climbWithConstraints(climberModel, mutation, moves):
+    check = False
+    while check == False:
+        for j in range(0, moves):
+            climbedModel = switchOrMove(mutation, climberModel)
+        check = climbedModel.checkValidity()
+    return climbedModel
+
+def climbWithoutConstraints(climberModel, mutation):
+    modelClimbed = switchOrMove(mutation, climberModel)
+    return modelClimbed
+
